c6c608be20617127d02210cb49df64b9826093b7
altheengineer <altheengineer@local>
2018-08-04 06:55:23 +0000

    lab1.v2, lab3.v1

--- lab1/text_recognizer/networks/mlp.py
+++ lab1/text_recognizer/networks/mlp.py
@@ -1,7 +1,7 @@
 from typing import Tuple
 from tensorflow.keras.models import Model, Sequential
 from tensorflow.keras.layers import Dense, Dropout, Flatten
-from tensorflow.keras.layers import Input, Add, Activation, ZeroPadding2D, BatchNormalization, Conv2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D
+from tensorflow.keras.layers import Input, Add, Activation, ZeroPadding2D, BatchNormalization, Conv2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D, Reshape
 from tensorflow.keras.applications import resnet50
 
 def identity_block(X, f, filters, stage, block):
@@ -30,19 +30,19 @@ def identity_block(X, f, filters, stage, block):
     X_shortcut = X
     
     # First component of main path
-    X = Conv2D(filters = F1, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2a', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = Conv2D(filters = F1, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2a')(X)
     X = BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
     X = Activation('relu')(X)
     
     ### START CODE HERE ###
     
     # Second component of main path (≈3 lines)
-    X = Conv2D(filters = F2, kernel_size = (f, f), strides = (1,1), padding = 'same', name = conv_name_base + '2b', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = Conv2D(filters = F2, kernel_size = (f, f), strides = (1,1), padding = 'same', name = conv_name_base + '2b')(X)
     X = BatchNormalization(axis = 3, name = bn_name_base + '2b')(X)
     X = Activation('relu')(X)
 
     # Third component of main path (≈2 lines)
-    X = Conv2D(filters = F3, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2c', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = Conv2D(filters = F3, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2c')(X)
     X = BatchNormalization(axis = 3, name = bn_name_base + '2c')(X)
 
     # Final step: Add shortcut value to main path, and pass it through a RELU activation (≈2 lines)
@@ -82,21 +82,21 @@ def convolutional_block(X, f, filters, stage, block, s = 2):
 
     ##### MAIN PATH #####
     # First component of main path 
-    X = Conv2D(filters = F1, kernel_size = (1, 1), strides = (s, s), padding = 'valid', name = conv_name_base + '2a', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = Conv2D(filters = F1, kernel_size = (1, 1), strides = (s, s), padding = 'valid', name = conv_name_base + '2a')(X)
     X = BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
     X = Activation('relu')(X)
     
     # Second component of main path (≈3 lines)
-    X = Conv2D(filters = F2, kernel_size = (f, f), strides = (1, 1), padding = 'same', name = conv_name_base + '2b', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = Conv2D(filters = F2, kernel_size = (f, f), strides = (1, 1), padding = 'same', name = conv_name_base + '2b')(X)
     X = BatchNormalization(axis = 3, name = bn_name_base + '2b')(X)
     X = Activation('relu')(X)
 
     # Third component of main path (≈2 lines)
-    X = Conv2D(filters = F3, kernel_size = (1, 1), strides = (1, 1), padding = 'valid', name = conv_name_base + '2c', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = Conv2D(filters = F3, kernel_size = (1, 1), strides = (1, 1), padding = 'valid', name = conv_name_base + '2c')(X)
     X = BatchNormalization(axis = 3, name = bn_name_base + '2c')(X)
 
     ##### SHORTCUT PATH #### (≈2 lines)
-    X_shortcut = Conv2D(filters = F3, kernel_size = (1, 1), strides = (s, s), padding = 'valid', name = conv_name_base + '1', kernel_initializer = glorot_uniform(seed=0))(X_shortcut)
+    X_shortcut = Conv2D(filters = F3, kernel_size = (1, 1), strides = (s, s), padding = 'valid', name = conv_name_base + '1')(X_shortcut)
     X_shortcut = BatchNormalization(axis = 3, name = bn_name_base + '1')(X_shortcut)
 
     # Final step: Add shortcut value to main path, and pass it through a RELU activation (≈2 lines)
@@ -114,15 +114,46 @@ def mlp(input_shape: Tuple[int, ...],
     Simple multi-layer perceptron: just fully-connected layers with dropout between them, with softmax predictions.
     Creates num_layers layers.
     """
+    image_height, image_width = input_shape
     num_classes = output_shape[0]
     
-    model = resnet50.ResNet50(weights=None, pooling='max', classes=num_classes)
+    image_input = Input(shape=input_shape)
+    
+    image_reshaped = Reshape((image_height, image_width, 1))(image_input)
+
+    
+    # Stage 1
+    X = Conv2D(64, (5, 5), padding='same', name = 'conv1')(image_reshaped)
+    X = Activation('relu')(X)
+    X = MaxPooling2D((2, 2), strides=(2, 2))(X)    
+    X = BatchNormalization(axis = 3, name = 'bn_conv1')(X)
+
+
+    X = Conv2D(128, (2, 2), padding='same', name = 'conv2')(X)
+    X = Activation('relu')(X)
+    X = MaxPooling2D((2, 2), strides=(2, 2))(X)
+    X = BatchNormalization(axis = 3, name = 'bn_conv2')(X)
+
+
+    # output layer
+    X = Flatten()(X)
+    X = Dense(1024, activation='relu')(X)
+    X = Dropout(0.5)(X)
+    X = Dense(512, activation='relu')(X)
+    X = BatchNormalization()(X)    
+    X = Dense(128, activation='relu')(X)
+    
+    preds = Dense(num_classes, activation='softmax')(X)
+    
+    
+    # Create model
+    model = Model(inputs=image_input, outputs=preds)
     
     ##### Your code above (Lab 1)
 
     return model
 
-"""
+"""MLP
     model = Sequential()
     # Don't forget to pass input_shape to the first layer of the model
     ##### Your code below (Lab 1)
@@ -132,4 +163,61 @@ def mlp(input_shape: Tuple[int, ...],
     model.add(Dense(256, activation='relu'))
     model.add(Dense(128, activation='relu'))
     model.add(Dense(output_shape[0], activation='softmax'))
+"""
+
+
+"""LeNet
+    X = Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(image_height, image_width, 1))(image_reshaped)
+    X = Conv2D(64, (3, 3), activation='relu')(X)
+    X = MaxPooling2D(pool_size=(2, 2))(X)
+    X = Dropout(0.2)(X)
+    X = Flatten()(X)
+    X = Dense(128, activation='relu')(X)
+    X = Dropout(0.2)(X)
+    preds = Dense(num_classes, activation='softmax')(X)
+    # Create model
+    model = Model(inputs=image_input, outputs=preds)
+"""
+
+"""ResNet50
+    # Zero-Padding
+    X = ZeroPadding2D((3, 3))(image_reshaped)
+    
+    # Stage 1
+    X = Conv2D(6, (3, 3), strides = (2, 2), name = 'conv1')(X)
+    X = BatchNormalization(axis = 3, name = 'bn_conv1')(X)
+    X = Activation('relu')(X)
+    X = MaxPooling2D((3, 3), strides=(2, 2))(X)
+
+    # Stage 2
+    X = convolutional_block(X, f = 3, filters = [6, 6, 18], stage = 2, block='a', s = 1)
+    X = identity_block(X, 3, [6, 6, 18], stage=2, block='b')
+    X = identity_block(X, 3, [6, 6, 18], stage=2, block='c')
+
+
+    # Stage 3 (≈4 lines)
+    X = convolutional_block(X, f = 3, filters = [12, 12, 36], stage = 3, block='a', s = 2)
+    X = identity_block(X, 3, [12, 12, 36], stage=3, block='b')
+    X = identity_block(X, 3, [12, 12, 36], stage=3, block='c')
+    X = identity_block(X, 3, [12, 12, 36], stage=3, block='d')
+
+    # Stage 4 (≈6 lines)
+    X = convolutional_block(X, f = 3, filters = [24, 24, 72], stage = 4, block='a', s = 2)
+    X = identity_block(X, 3, [24, 24, 72], stage=4, block='b')
+    X = identity_block(X, 3, [24, 24, 72], stage=4, block='c')
+    X = identity_block(X, 3, [24, 24, 72], stage=4, block='d')
+    X = identity_block(X, 3, [24, 24, 72], stage=4, block='e')
+    X = identity_block(X, 3, [24, 24, 72], stage=4, block='f')
+
+    # Stage 5 (≈3 lines)
+    X = convolutional_block(X, f = 3, filters = [48, 48, 144], stage = 5, block='a', s = 2)
+    X = identity_block(X, 3, [48, 48, 144], stage=5, block='b')
+    X = identity_block(X, 3, [48, 48, 144], stage=5, block='c')
+
+    # AVGPOOL (≈1 line). Use "X = AveragePooling2D(...)(X)"
+    # X = AveragePooling2D(pool_size=(2, 2), name='avg_pool')(X)
+    
+    # output layer
+    X = Flatten()(X)
+    preds = Dense(num_classes, activation='softmax')(X)
 """
--- lab3/text_recognizer/networks/lenet.py
+++ lab3/text_recognizer/networks/lenet.py
@@ -6,22 +6,42 @@ from tensorflow.keras.models import Sequential, Model
 
 
 def lenet(input_shape: Tuple[int, ...], output_shape: Tuple[int, ...]) -> Model:
+    
+    image_height, image_width = input_shape
+    
     num_classes = output_shape[0]
+    
+    image_input = Input(shape=input_shape)
+    
+    image_reshaped = Reshape((image_height, image_width, 1))(image_input)
 
-    ##### Your code below (Lab 2)
-    model = Sequential()
-    if len(input_shape) < 3:
-        model.add(Lambda(lambda x: tf.expand_dims(x, -1), input_shape=input_shape))
-        input_shape = (input_shape[0], input_shape[1], 1)
-    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.2))
-    model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(num_classes, activation='softmax'))
-    ##### Your code above (Lab 2)
+    
+    # Conv1
+    X = Conv2D(64, (5, 5), padding='same', name = 'conv1')(image_reshaped)
+    X = Activation('relu')(X)
+    X = MaxPooling2D((2, 2), strides=(2, 2))(X)    
+    X = BatchNormalization(axis = 3, name = 'bn_conv1')(X)
+
+    # Conv2
+    X = Conv2D(128, (2, 2), padding='same', name = 'conv2')(X)
+    X = Activation('relu')(X)
+    X = MaxPooling2D((2, 2), strides=(2, 2))(X)
+    X = BatchNormalization(axis = 3, name = 'bn_conv2')(X)
+
+
+    # Output FC layers
+    X = Flatten()(X)
+    X = Dense(1024, activation='relu')(X)
+    X = Dropout(0.5)(X)
+    X = Dense(512, activation='relu')(X)
+    X = BatchNormalization()(X)    
+    X = Dense(128, activation='relu')(X)
+    
+    preds = Dense(num_classes, activation='softmax')(X)
+    
+    
+    # Create model
+    model = Model(inputs=image_input, outputs=preds)
 
     return model
 
