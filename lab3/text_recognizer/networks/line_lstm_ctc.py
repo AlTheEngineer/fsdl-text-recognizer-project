27192833507eddea996a6e2af6506ec66c0679d5
altheengineer <altheengineer@local>
2018-08-04 08:01:11 +0000

    lab3.v2

--- lab3/text_recognizer/networks/line_lstm_ctc.py
+++ lab3/text_recognizer/networks/line_lstm_ctc.py
@@ -50,10 +50,11 @@ def line_lstm_ctc(input_shape, output_shape, window_width=28, window_stride=14):
     convnet_outputs = TimeDistributed(convnet)(image_patches)
     # (num_windows, 128)
     # feed lenet outputs to a LSTM (instead of sliding windows). Try bidirectional and wider layers
-    lstm_output = Bidirectional(lstm_fn(128, return_sequences=True))(convnet_outputs)
+    lstm_output1 = Bidirectional(lstm_fn(256, return_sequences=True))(convnet_outputs)
+    lstm_output2 = Bidirectional(lstm_fn(128, return_sequences=True))(lstm_output1)
     # (num_windows, 128)
 
-    softmax_output = Dense(num_classes, activation='softmax', name='softmax_output')(lstm_output)
+    softmax_output = Dense(num_classes, activation='softmax', name='softmax_output')(lstm_output2)
     # (num_windows, num_classes)
     ##### Your code above (Lab 3)
 
